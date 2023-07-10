44f36a09ccf227e1839275cac6a1dad83c99683b
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2023-07-11 00:08:32 +0530

    Create single_num.c

---
+++ bit manipulation/single_num.c
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/single-number/
+#include<unordered_map>
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        for(auto i:nums)
+        {
+            umap[i]++;
+        }
+        for(auto i:umap)
+        {
+            if (i.second == 1)
+                return i.first;
+        }
+        return 0;
+    }
+};
