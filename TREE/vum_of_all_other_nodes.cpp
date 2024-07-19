cbf172fa2bec39d0ac0530be6038b7c2ec36d259
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-07-19 13:23:06 +0530

    Create vum_of_all_other_nodes.cpp

---
+++ TREE/vum_of_all_other_nodes.cpp
@@ -0,0 +1,15 @@
+Ex Input.
+       4
+     /   \
+   2     6
+  / \    /
+ 1   3  5
+
+
+Given a Binary Search Tree transform it in to a tree where each node will have the value as sum of all other nodes which are greater than the current node value.
+Ex. Output
+       11
+      /  \
+    18    0
+   /  \  /
+  20  15 6
