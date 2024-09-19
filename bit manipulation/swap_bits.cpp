2fb7263f4ef7ae67d9a177470c4d0824db19223b
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-09-19 23:16:19 +0530

    Create swap_bits.cpp

---
+++ bit manipulation/swap_bits.cpp
@@ -0,0 +1,5 @@
+//two swap 2 bit:
+//Extract the bits at p1 and p2 by using bitwise operations.
+//If the bits are different, flip them by toggling the bits at p1 and p2 using XOR (^).
+//(num ^= (1 << p1))
+
