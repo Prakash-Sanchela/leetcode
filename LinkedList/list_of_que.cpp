941076544f2b7c2afdc6c9dda1baa51c7485c5b6
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-10-01 00:22:47 +0530

    Create list_of_que.cpp

---
+++ LinkedList/list_of_que.cpp
@@ -0,0 +1,20 @@
+Palindrome Linked List
+Swapping Nodes in a Linked List
+Odd Even Linked List
+Add Two Numbers
+Swap Nodes in Pairs
+Split Linked List in Parts
+Insertion sort on Linked List
+Merge sort on Linked List
+Copy list with random pointers
+Remove zero sum from consecutive nodes from linked list
+Merge k sorted Linked List
+Reverse nodes in k group
+Doubly Linked List
+Adding a node at the front, at the end, after a node or before a node
+Deleting a node from the front, from the end, after a node or before a node
+Circular Doubly Linked List
+Adding a node at the front, at the end, after a node or before a node
+Deleting a node from the front, from the end, after a node or before a node
+LRU Cache
+LFU Cache
