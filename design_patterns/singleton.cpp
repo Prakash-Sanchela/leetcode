97c8a07458f44be635a1a0559bf4b231408fa5a2
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-10-14 12:58:53 +0530

    Update singleton.cpp

--- design_patterns/singleton.cpp
+++ design_patterns/singleton.cpp
@@ -1,1 +1,38 @@
 
+4. Thread safe Singleton
+#include <iostream>
+#include <mutex>
+using namespace std;
+// thread safe singleton
+class singleton
+{
+private :
+    static singleton *st_ptr;
+    singleton()
+    {
+        cout << "singleton constructor " << endl;
+    }
+    singleton(const singleton& obj) = delete;
+    singleton& operator=(const singleton& obj) = delete;
+public:
+    static singleton& getInstance()
+    {
+        if (!st_ptr)
+        {
+            mutex _mutex;
+            lock_guard<mutex> lock(_mutex);
+            if (!st_ptr)
+            {   
+                st_ptr = new singleton();
+            }
+        }
+        return *st_ptr;
+    }
+};
+singleton* singleton::st_ptr = nullptr;
+int main()
+{
+    cout<<"Hello World";
+    
+    return 0;
+}
