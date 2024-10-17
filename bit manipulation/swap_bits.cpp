54723ed1289c4ce2543f10af8e7b81b8c2ec9429
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-10-17 13:52:03 +0530

    Update swap_bits.cpp

--- bit manipulation/swap_bits.cpp
+++ bit manipulation/swap_bits.cpp
@@ -3,3 +3,18 @@
 //If the bits are different, flip them by toggling the bits at p1 and p2 using XOR (^).
 //(num ^= (1 << p1))
 
+void swap_bit(int num, int src_pos, int dest_pos, int no_of_bit)
+{
+	//initialization
+	int src_num = dest_num = 0;
+	for(int index = 0; index < 32; index++)
+	{
+		for(int in_index = 0; in_index < no_of_bit; in_index++)
+		{
+			// separate numbers bit by bit
+			src_num = num & (1 << src_pos++); // src bit check starting
+			dst_num = num & (1 << dest_num++); // dst bit check starting		
+		}
+	}
+}
+
