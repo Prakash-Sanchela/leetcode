18556c21cd022a83756e1d8f101227284234f85b
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-02-06 00:26:22 +0530

    Create siemens_advant.cpp

---
+++ intervew_experience/siemens_advant.cpp
@@ -0,0 +1,48 @@
+#03/02/2025
+Interviewer Name - Gaurang Dudhat
+
+Intro
+How does routing table looks like? What are the information availble in routing table
+How does router transfer one packet from interface to another interface?
+When two host are on another network, how does they talk, how packet transfered from one host to another host?
+What is PING?
+  - how does ICMP request response looks like?
+  - How ARP works
+  - ARP packet
+How are you connecting to your product?
+  - via HTTPS
+  - Explain how HTTPS connection happens
+List out the server side APIs used for TCP and UDP socket
+
+##C++
+What is the difference between C & C++?
+Encapsulation vs Abstraction
+Static in C & C++
+lock_guard syntax
+thread_safe singleton
+Example of using singleton
+Write copy constructor and copy assignment operator
+Overload any C++ operator
+Casting scenario  
+  class base
+  class derived1 : public base
+  class derived2 : public base
+
+  base *ptr = new derived1();
+  derived2 obj = ptr; // is this possible, if not how to prevent this situation.
+
+#write a program to create 2D array
+  
+##cryptography
+Do you know cryptography?
+
+##linux
+Command to search the string in specific directory
+Let's say you have a USB and linux machine only, how will you copy the file from USB to linux machine
+    - mount/unmount
+What is namespace and what can we achieve using namespace
+what is docker?
+namespace vs docker
+
+#Git 
+git checkout vs git switch
