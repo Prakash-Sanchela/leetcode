ba8ff93f0e1f83973d73a7705cc379126e5bcbdb
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-05-18 15:58:00 +0530

    Create whatsapp.cpp

---
+++ system_design/whatsapp.cpp
@@ -0,0 +1,10 @@
+# Requirements
+1. One to once message
+  - Text message
+  - Image sharing
+  - Video sharing 
+  - (No need to store for privacy purpose)
+2. Group messagging 
+  - Should be scalable
+  - Let's say initially it supports 10 member in a group later could be more than 100
+3. Send, delivered and read receipts. 
