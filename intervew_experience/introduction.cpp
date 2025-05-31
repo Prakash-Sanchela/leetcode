975a03a5e2f62c84c4c5ca7e3a622ddff01bcd88
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-05-31 18:31:16 +0530

    Create introduction.cpp

---
+++ intervew_experience/introduction.cpp
@@ -0,0 +1,8 @@
+Hi, I’m currently working as a Principal Development Engineer at Calsoft, where I’ve been for the past 3 years, primarily in the storage and networking domain.
+My day-to-day work involves C, C++, and Python on Linux, and I’ve contributed to projects like developing Redfish-compliant APIs for server management and control plane development for SONiC, the open-source networking OS.
+
+Before that, I was with Dassault Systèmes for almost 3 years, working on PLM solutions — specifically customizing CATIA v6 for Naval Group. I handled both feature development and performance improvements using C++ on Windows, and even got to implement a cache mechanism that boosted performance by around 20%.
+
+I started my career at NCR Corporation, where I worked in the retail domain — building POS and system imaging utilities. That’s where I got my foundation in product development, software packaging, and client interactions.	
+
+Across all roles, I’ve been hands-on with low-level system programming, performance tuning, and cross-platform development, and I’m passionate about solving real-world problems with clean, efficient code.
