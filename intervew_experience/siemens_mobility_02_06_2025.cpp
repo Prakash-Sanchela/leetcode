29945908692f76280bfcd8031df17d0b8f2fd141
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-06-04 02:56:13 +0530

    Create siemens_mobility_02_06_2025.cpp

---
+++ intervew_experience/siemens_mobility_02_06_2025.cpp
@@ -0,0 +1,3 @@
+What challange compiler get's while using return type in function overloading?
+Please explain when you use base class pointer with derived class object how it maps to the respective function in case of virtual functions?
+write design pattern
