9ca0078173851858bac2a271bb64169584af78ab
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-06-24 22:36:08 +0530

    Create data_axel.cpp

---
+++ intervew_experience/data_axel.cpp
@@ -0,0 +1,9 @@
+1. How will you scale any application?
+  - Let's say you have an application and that application should be scalable and based on customer requirement load can increase
+    and decrease at run time. 
+  - How will you scale and what all functionality you'll check. 
+2. How will you design the cache?
+  - What all parameters you would consider for cache and what multiple layer you can add. 
+  - Data sharding
+  - LRU
+3. steps to debug the core dump of an application
