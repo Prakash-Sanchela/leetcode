c1dc2b97dc6f9cbb2e33951a3094bba5a9e60af8
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:19:14 +0530

    Create true_minds.cpp

---
+++ intervew_experience/true_minds.cpp
@@ -0,0 +1,22 @@
+	1. Linked list :Let's say you have billions of single link list and you will not get head of the list.
+	As we have billions of node we don't want to traverse complete list to delete the single node. 
+	Instead you'll be given address of that node which needs to be deleted. How will you delete
+	2. Bit manipulation : Swap bits of 32 bit integer. 
+	E.g. Input(int num, int starting_bit, int starting_bit_to_swap, int number of bits to swap)
+	Input :48, 2,5,2 : bit 2-3 needs to be swapped with bit 5-6. 
+	ip-0011 0000
+	
+	op-0001 0100
+	3. Bit manipulation : Reset the perticular bit position on number
+	5- 0000 0101 reset the first bit
+	4. Remove duplicates from the string
+	5. Linked list : Remove nth node from the end
+	6. Storage classes in C
+	7. Memory organization in C
+	8. Global vs static
+	9. How to know the chrome traffic is using quic protocol or not
+	10. Why quic is faster then tcp/udp. How chrome decides should I use quic or not?
+	11. How MPTCP works?
+	12. Networking : Two systems connected to switch A<->switch<->B. 
+	If A wants to send data to B what is the process. 
+	Does IP involved here? If yes switch works on L3 how does it involved IP
