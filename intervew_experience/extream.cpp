9c636b5a70f5a78873ad33cea261b6a84f133df0
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:17:11 +0530

    Create extream.cpp

---
+++ intervew_experience/extream.cpp
@@ -0,0 +1,20 @@
+1. How dpdk imporves the performace
+2. how your data flows using DPDK
+3. in dpdk what are the criteria for poll-mode drivers
+4. how frequently poll-mode drivers checkes on the ring buffer, what's the speed of picking up packet per second.
+5. tree program  
+6. time complexity calculation
+7. Have you used multithreading 
+8. are you using any posix threads?
+9. how are you synchronizing the threads while sharing the data.
+10. what mutex APIs are you using for synchronizing.
+11. do you know any other data plane other then ovs and dpdk?
+12. Tell me something about L3 packet processing 
+13. which networking layer you're familier with (tell me about L3 packet processing)
+14. endians apis
+	how to identify endians 
+15. Race conditions
+14. What is your project architecture.
+15. are you using any microservices.
+16. if your modules running as services how they're communicating. 
+17. convert 0x27 to 0x27272727 (hint using bitwise operators)
