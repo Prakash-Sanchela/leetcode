e314aa4f382c39e3be8d4689da3d700a3e378a44
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:16:23 +0530

    Create dassault.cpp

---
+++ intervew_experience/dassault.cpp
@@ -0,0 +1,9 @@
+	1. Memory management
+	2. What is abstract class? How to make it abstract class? Using pure virtual functions
+	3. Is there any alternate way to make abstract apart from pure virtual?
+	4. What are the OOPS you have used in your project?
+	5. How to create a reference counter in which we need to increment the counter whenever one object is being assigned to the pointer to that class. Delete should not be allowed until and unless the counter reaches to 1.
+	
+	6. 10 coin 10 grams and faulty machine produced wrong weight coin puzzle
+	7. Rectangle cake, how many cuts we can make an 8 equal pieces of cake.
+
