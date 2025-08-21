2f1d3aa63446fcfe3e17fb132a057a552304b5c9
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:22:41 +0530

    Create omnisaa.cpp

---
+++ intervew_experience/omnisaa.cpp
@@ -0,0 +1,31 @@
+	1. How free and malloc works All sorts of theory questions on C , C++ and OS 2 dsa question 1 on linked lists the other on string manipulation
+	2. Serialize and deserialize a binary tree (used a priority queue) - 
+	3. Design a hash module and make search efficient (LLD) - 
+	4. Design connection ranking system of LinkedIn (level 1 connection, level 2 connection etc) [HLD] 
+	5. Questions about work and behavioral questions
+	6. LRU cache implementation
+	7. Merge 2 sorted arrays 
+	8. Reverse linkedlist
+	9. print binary tree elements in anti clock wise (only outer elements - left most, bottom and right most)
+	10. Given list of customers with time taken to bill and number of billing machines, calculate minimum time required to bill all the customers customersTimeTaken = [4,2,5,7,9,1,2,3] noOfBillingMachines = 3
+	11. merge 2 sorted linkedlist in reverse order without using extra space and without reversing
+	12. Find missing and repeating number.
+	13. Stock buy and sell
+	14. Find middle of linked list 
+	15. Basic Tree operations
+	16. implement hash map
+	17. Reverse a sentence using stack
+	Breadth first search algorithm
+	
+Basic Tree programs
+Implemented Hash tables
+Repeat SOLID
+Check LPE design and class diagrams
+	
+Techno-Managerial round, 
+	OOPS, Project, Coding Round 4 : Questions related to Project, OOPS, Behavioral Questions and one puzzle
+	
+=======================================
+	1. Implement shared_ptr
+	2. Task scheduler
+	3. Reverse the stack without using extra space, o(1) constant space is allowed.
