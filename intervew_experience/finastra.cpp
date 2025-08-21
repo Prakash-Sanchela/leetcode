85512ca62ad2ac8c0632cecaecb9961ecc7fad51
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 00:18:59 +0530

    Create finastra.cpp

---
+++ intervew_experience/finastra.cpp
@@ -0,0 +1,13 @@
+1. Write a code for deep copy
+2. How do we know static_cast fails, where the error comes
+3. What will dynamic_cast will return if fails in case of reference
+4. Stack in any process
+5. Thread vs process
+6. How thread will send a signal to process or other thread that I am done.
+7. How to increase the size of heap at run time, in case I need a more memory
+8. Compiler name of visual studio
+9. Name mangling in C, how and what compiler gives the name to each function
+10. When you want to update any application, what steps you'll follow ? In context of dlls.
+11. How to initialize const var in C++
+12. When to use strategy design pattern 
+13. What is the difference in Factory and strategy design pattern
