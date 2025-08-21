5f9fc24de7bba34841fa429ce366585068092cfd
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2025-08-22 01:36:50 +0530

    Create uniKen.cpp

---
+++ intervew_experience/uniKen.cpp
@@ -0,0 +1,14 @@
+1. How to debug if connection is not establishing?
+2. How to debug from server side if client request is not able to read?
+3. How communication is being done from web server to rest server?
+	- is it SOCKET or is it web socket
+4. What is REST & alternative to REST?
+5. What HTTPS version you're using
+6. When server clients are working, how to check if server is up and not. 
+	- TELNET need to check what port is up or not?
+7. Idea if RAII
+8. Types of Memory
+9. What all design pattern you've used?
+10. Divide your project and check how each modules communicate with each other?
+11. Which design patter this redfish uses?
+12. How DNS works?
